cf272dd07c77f83bd7c08254ca1e10e9f1ee8ab2
cristobaltm <cristobaltm@gmail.com>
2020-08-08 18:32:52 +0200

    71. Configuración del backend - Encuestas

--- 03-server-multi/classes/graph.ts
+++ 03-server-multi/classes/graph.ts
@@ -1,25 +1,51 @@
 export class GraphData {
 
-    private months: string[];
+    private labels: string[];
     private values: number[];
 
     constructor() {
-        this.months = ['enero', 'febrero', 'marzo', 'abril'];
+        this.labels = ['Opción 1','Opción 2','Opción 3','Opción 4'];
         this.values = [0, 0, 0, 0];
     }
 
+    /**
+     * Establecer el valor de las etiquetas.
+     * @param labels Array con las etiquetas.
+     */
+    setLabels( labels: string[] ) {
+        this.labels = labels;
+    }
+
+    /**
+     * Devolver los datos de la gráfica como objeto.
+     */
     getGraphData() {
         return [{
             data: this.values,
-            label: this.months
+            label: this.labels
         }];
     }
 
-    increaseValue( month: string, value: number ) {
-        month = month.toLowerCase().trim();
+    /**
+     * Incrementar el valor indicado de la opción elegida.
+     * @param option Posición de la opción a incrementar.
+     * @param valor Valor a incrementar (o decrementar si es negativo).
+     */
+    increaseValue( option: number, valor: number ) {
+        this.values[option] += valor;
+        return this.getGraphData();
+    }
 
-        for ( let i in this.months ) {
-            if ( this.months[ i ] === month ) {
+    /**
+     * Incrementar el valor indicado la opción coincidente con la etiqueta.
+     * @param label Etiqueta a incrementar.
+     * @param value Valor a incrementar (o decrementar si es negativo).
+     */
+    increaseValueByLabel( label: string, value: number ) {
+        label = label.toLowerCase().trim();
+
+        for ( let i in this.labels ) {
+            if ( this.labels[ i ] === label ) {
                 this.values[ i ] += value;
             } 
         }
--- 03-server-multi/routes/router.ts
+++ 03-server-multi/routes/router.ts
@@ -12,33 +12,39 @@ router.get( '/graph', ( req: Request, res: Response ) => {
 
 router.post( '/graph', ( req: Request, res: Response ) => {
 
-    const month = req.body.month;
-    const value = Number( req.body.value );
-
-    graph.increaseValue( month, value );
+    const option = Number( req.body.option );
+    const value = Number( req.body.value ) || 1;
     const server = Server.instance;
+
+    graph.increaseValue( option, value );
     server.io.emit( 'change-graph', graph.getGraphData() );
 
     res.json( graph.getGraphData() );
 });
 
-router.post( '/mensajes/:id', ( req: Request, res: Response ) => {
+router.post( '/graph/bylabel', ( req: Request, res: Response ) => {
+
+    const label = req.body.label;
+    const value = Number( req.body.value );
+    const server = Server.instance;
+
+    graph.increaseValueByLabel( label, value );
+    server.io.emit( 'change-graph-by-label', graph.getGraphData() );
+
+    res.json( graph.getGraphData() );
+});
+
+router.post( '/messages/:id', ( req: Request, res: Response ) => {
 
     const body = req.body.body;
     const from = req.body.from;
     const id = req.params.id;
-
-
     const payload = { from, body }
     const server = Server.instance;
+
     server.io.in( id ).emit( 'private-message', payload );
 
-    res.json({
-        ok: true,
-        body,
-        from,
-        id
-    });
+    res.json({ ok: true, body, from, id });
 });
 
 export default router;
